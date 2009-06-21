c3908ebfc05627c29754ee7299e1fcba8a334976
(no author) <(no author)@508b99f4-6b71-4659-bec4-6d750112b50a>
2009-06-21 12:28:41 +0000

    mywire2
    git-svn-id: svn://fw.home.toril.ru/mywire2@890 508b99f4-6b71-4659-bec4-6d750112b50a

---
+++ src/main/scala/info/akshaal/mywire2/daemon/Daemon.scala
@@ -0,0 +1,5 @@
+package info.akshaal.mywire2.daemon
+
+object Daemon {
+    println ("hello world")
+}
---
+++ src/test/scala/info/akshaal/mywire2/test/unit/daemon/DaemonTest.scala
@@ -0,0 +1,12 @@
+package info.akshaal.mywire2.test.unit.daemon
+
+import info.akshaal.mywire2.daemon.Daemon
+import org.testng.annotations.Test;
+
+@Test
+class DaemonTest {
+    @Test
+    def testDaemon () =
+      println ("Test!")
+      Daemon
+}
