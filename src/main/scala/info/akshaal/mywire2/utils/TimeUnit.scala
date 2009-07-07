303696a1455bce36967649fb7418186f5e9ab2fd
(no author) <(no author)@508b99f4-6b71-4659-bec4-6d750112b50a>
2009-07-07 06:05:24 +0000

    use lazy vals
    git-svn-id: svn://fw.home.toril.ru/mywire2@917 508b99f4-6b71-4659-bec4-6d750112b50a

--- src/main/scala/info/akshaal/mywire2/utils/TimeUnit.scala
+++ src/main/scala/info/akshaal/mywire2/utils/TimeUnit.scala
@@ -8,11 +8,11 @@
 package info.akshaal.mywire2.utils
 
 final class TimeUnit (nano : Long) {
-    def asNanoseconds  = nano
-    def asMicroseconds = nano / 1000L
-    def asMilliseconds = nano / 1000L / 1000L
-    def asSeconds      = nano / 1000L / 1000L / 1000L
-    def asMinutes      = nano / 1000L / 1000L / 1000L / 60L
-    def asHours        = nano / 1000L / 1000L / 1000L / 60L / 60L
-    def asDays         = nano / 1000L / 1000L / 1000L / 60L / 60L / 24L
+    lazy val asNanoseconds  = nano
+    lazy val asMicroseconds = nano / 1000L
+    lazy val asMilliseconds = nano / 1000L / 1000L
+    lazy val asSeconds      = nano / 1000L / 1000L / 1000L
+    lazy val asMinutes      = nano / 1000L / 1000L / 1000L / 60L
+    lazy val asHours        = nano / 1000L / 1000L / 1000L / 60L / 60L
+    lazy val asDays         = nano / 1000L / 1000L / 1000L / 60L / 60L / 24L
 }
