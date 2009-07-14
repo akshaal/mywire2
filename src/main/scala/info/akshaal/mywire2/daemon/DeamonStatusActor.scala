51e7145893b9f292c31416947798463d890ad245
(no author) <(no author)@508b99f4-6b71-4659-bec4-6d750112b50a>
2009-07-14 17:03:37 +0000

    tuning
    git-svn-id: svn://fw.home.toril.ru/mywire2@922 508b99f4-6b71-4659-bec4-6d750112b50a

--- src/main/scala/info/akshaal/mywire2/daemon/DeamonStatusActor.scala
+++ src/main/scala/info/akshaal/mywire2/daemon/DeamonStatusActor.scala
@@ -4,7 +4,7 @@ package info.akshaal.mywire2.daemon
 import info.akshaal.mywire2.actor.{MywireActor, LowPriorityPool}
 
 object DeamonStatusActor extends MywireActor with LowPriorityPool {
-    schedule payload 'DoIt every 30 seconds
+    schedule payload 'DoIt every 3 seconds
 
     def act () = {
         case 'DoIt => {
