44de037ca3a63ae2e7b3752653130ca8972133d1
(no author) <(no author)@508b99f4-6b71-4659-bec4-6d750112b50a>
2009-07-22 18:20:29 +0000

    ...
    git-svn-id: svn://fw.home.toril.ru/mywire2@948 508b99f4-6b71-4659-bec4-6d750112b50a

---
+++ src/test/scala/info/akshaal/mywire2/system/test/unit/BaseUnitTest.scala
@@ -0,0 +1,52 @@
+/*
+ * BaseTest.scala
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package info.akshaal.mywire2.system.test.unit
+
+import java.lang.reflect.Method
+
+import org.testng.annotations.{AfterMethod,
+                               BeforeMethod,
+                               AfterSuite,
+                               BeforeSuite}
+
+import mywire2.system.RuntimeConstants
+import mywire2.system.logger.Logging
+
+/**
+ * Basic initialization.
+ */
+private[unit] object UnitTestState extends Logging {
+    var ready = false
+
+    def init () {
+        synchronized {
+            if (!ready) {
+                doInit
+                ready = true
+            }
+        }
+    }
+
+    private def doInit () {
+        UnitTestModule // Touching
+
+        info ("Initializing")
+    }
+}
+
+/**
+ * Base class for all tests.
+ */
+class BaseUnitTest extends BaseTest {
+    @BeforeSuite
+    override def beforeSuite () {
+        UnitTestState.init
+        
+        super.beforeSuite
+    }
+}
---
+++ src/test/scala/info/akshaal/mywire2/system/test/unit/UnitTestModule.scala
@@ -0,0 +1,16 @@
+/*
+ * TestModule.scala
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package info.akshaal.mywire2.system.test.unit
+
+import mywire2.system.logger.LogActor
+
+object UnitTestModule {
+    object LogActorImpl extends LogActor {
+        
+    }
+}
