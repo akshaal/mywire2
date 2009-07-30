75a1e101371fbb46d400ee8be190df5f6524701f
(no author) <(no author)@508b99f4-6b71-4659-bec4-6d750112b50a>
2009-07-30 18:49:31 +0000

    ...
    git-svn-id: svn://fw.home.toril.ru/mywire2@965 508b99f4-6b71-4659-bec4-6d750112b50a

--- src/main/scala/info/akshaal/mywire2/system/daemon/DeamonStatusActor.scala
+++ src/main/scala/info/akshaal/mywire2/system/daemon/DeamonStatusActor.scala
@@ -1,15 +1,31 @@
 
-package info.akshaal.mywire2.system
+package info.akshaal.mywire2
+package system
 package daemon
 
+import Predefs._
 import actor.Actor
+import utils.{TimeUnit, NormalPriorityPool}
 
-private[daemon] abstract class DeamonStatusActor extends Actor {
-    schedule payload 'DoIt every RuntimeConstants.daemonStatusUpdateInterval
+private[system] abstract class DeamonStatusActor extends Actor {
+    schedule payload UpdateStatus every interval
 
+    protected val pool : NormalPriorityPool
+
+    protected val interval : TimeUnit
+
+    protected val daemonStatus : DaemonStatus
+
+    protected val statusFile : String
+
+    /**
+     * Process messages
+     */
     final def act () = {
-        case 'DoIt => {
+        case UpdateStatus => {
             // TODO: Implement it
         }
     }
+
+    private final case object UpdateStatus
 }
