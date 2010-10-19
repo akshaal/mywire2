43ce5e9e4956ae7df64dd0e415c6af78e6a02c21
Evgeny Chukreev <Evgeny.Chukreev@gmail.com>
2010-10-19 21:55:44 +0300

    Test conditions for SimpleOnOffStrategy relaxed.

--- src/test/scala/info/akshaal/mywire2/test/unit/strategy/StrategiesTest.scala
+++ src/test/scala/info/akshaal/mywire2/test/unit/strategy/StrategiesTest.scala
@@ -14,32 +14,33 @@ import strategy.SimpleOnOffStrategy
 class StrategiesTest extends JacoreSpecWithJUnit ("Strategies specification") {
     "SimpleOnOffStrategy" should {
         "work properly" in {
-            val s = new SimpleOnOffStrategy (onInterval = 50 milliseconds,
-                                             offInterval = 30 milliseconds)
+            val onTime = 200 milliseconds
+            val offTime = 120 milliseconds
+            val s = new SimpleOnOffStrategy (onInterval = onTime, offInterval = offTime)
 
             // Wait till it is on + 15ms
             val startStateUpdate = s.getStateUpdate
             if (startStateUpdate.state == false) {
                 Thread.sleep (startStateUpdate.validTime.asMilliseconds + 15)
             } else {
-                Thread.sleep (startStateUpdate.validTime.asMilliseconds + 15 + 30)
+                Thread.sleep ((startStateUpdate.validTime + offTime).asMilliseconds + 15)
             }            
 
             // Do testing
             s.getStateUpdate.state  must_== true
             (!s.getStateUpdate).state  must_== false
 
-            Thread.sleep (50)
+            Thread.sleep (onTime.asMilliseconds)
 
             s.getStateUpdate.state  must_== false
             (!s.getStateUpdate).state  must_== true
 
-            Thread.sleep (30)
+            Thread.sleep (offTime.asMilliseconds)
 
             s.getStateUpdate.state  must_== true
             (!s.getStateUpdate).state  must_== false
 
-            Thread.sleep (50)
+            Thread.sleep (onTime.asMilliseconds)
 
             s.getStateUpdate.state  must_== false
             (!s.getStateUpdate).state  must_== true
